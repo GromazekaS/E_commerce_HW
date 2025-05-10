b659ab366d8b1d344cb7f5f9468b328839bcd609
GromazekaS <kinst41@gmail.com>
2025-05-10 15:39:47 +0300

    done Product, Category for functionality in main.py

---
+++ src/classes.py
@@ -0,0 +1,31 @@
+from itertools import product
+
+
+class Product:
+    name : str
+    description : str
+    price : float
+    quantity: int
+
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    name : str
+    description : str
+    products : list[Product]
+    category_count : int = 0
+    product_count : int = 0
+
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        self.product_count += len(products)
+        Category.category_count += 1
