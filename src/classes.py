8b08bc0e6e4c15e12b0a7c2ff06fc97f45b8de6d
GromazekaS <kinst41@gmail.com>
2025-05-11 14:20:55 +0300

    done 1, 2, 3, 3* tasks

--- src/classes.py
+++ src/classes.py
@@ -1,8 +1,14 @@
+from tests.conftest import products_list
+from pprint import pprint
+
+
 class Product:
     name: str
     description: str
     price: float
     quantity: int
+    product_names: list[str]
+    products_list: list
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         """Инициализация объекта класса Product"""
@@ -11,11 +17,23 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, kwargs, search_list: list | None = None ):
+        """Добавление нового продукта с проверкой наличия такого же в списке"""
+        if search_list:
+            for p in search_list:
+                if p.name == kwargs['name']:
+                    p.quantity += kwargs['quantity']
+                    p.price = max(p.price, kwargs['price'])
+                    return p
+        return Product(**kwargs)
+
+
 
 class Category:
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
     product_count: int = 0
     category_count: int = 0
 
@@ -23,6 +41,21 @@ class Category:
         """Инициализация объекта класса Category"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.product_count += len(products)
         Category.category_count += 1
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        result = ""
+        for product in self.__products:
+            result += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return result
+
+    @property
+    def search_list(self) -> list[Product]:
+        return self.__products
