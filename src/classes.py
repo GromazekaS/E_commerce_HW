8cd038f1e4e8750c9cc4c67aba02572835bd0c31
GromazekaS <kinst41@gmail.com>
2025-05-14 19:47:18 +0300

    task 1 done

--- src/classes.py
+++ src/classes.py
@@ -56,6 +56,32 @@ class Product:
             print("Цена не должна быть нулевая или отрицательная")
 
 
+class Smartphone(Product):
+    efficiency: float
+    model: str
+    memory: int
+    color: str
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+class LawnGrass(Product):
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+
+
 class Category:
     name: str
     description: str
